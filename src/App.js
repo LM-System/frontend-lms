a12e0b4a1928ea54992eb2186b8ec418a4de052d
Helmi-Qatqat <helmiqatqat@gmail.com>
2023-05-24 11:30:05 +0300

    folders initialized, added error page

--- src/App.js
+++ src/App.js
@@ -1,29 +1,38 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Sidebar from './components/Sidebar/Sidebar';
 import Main from './components/Main/Main';
+import Error from './components/assets/Error/Error';
+import {Routes, Route} from 'react-router-dom'
 
 function App() {
   const [isSidebarVisible, setSidebarVisibility] = useState(false)
   const [isLogin, setIsLogin] = useState(true)
-  const [darkMode, setDarkMode] = useState(localStorage.getItem('darkMode') || false)
+  const [darkMode, setDarkMode] = useState(false)
+
   function darkModeToggle() {
     setDarkMode(prevState => !prevState);
-    localStorage.setItem('darkMode', darkMode)
   }
+
   return (
     <div className='body-container'>
+      <Routes>
+        {/* <Route path='*' element={<Error/>}/> */}
+        <Route path='*' element={
+          <>
+          <Sidebar
+            isSidebarVisible={isSidebarVisible}
+            darkMode={darkMode}/>
+          <Main
+            setIsLogin={setIsLogin}
+            isSidebarVisible={isSidebarVisible}
+            setSidebarVisibility={setSidebarVisibility}
+            darkMode={darkMode}
+            darkModeToggle={() => darkModeToggle()}/>
+          </>
+        }/>
+      </Routes>
       
-      {isLogin && <Sidebar
-      isSidebarVisible={isSidebarVisible}
-      darkMode={darkMode}/>}
-
-      {isLogin && <Main
-      setIsLogin={setIsLogin}
-      isSidebarVisible={isSidebarVisible}
-      setSidebarVisibility={setSidebarVisibility}
-      darkMode={darkMode}
-      darkModeToggle={() => darkModeToggle()}/>}
     </div>
   );
 }
---
+++ src/components/assets/Error/Error.js
@@ -0,0 +1,15 @@
+import './Error.css'
+import { Link } from 'react-router-dom'
+export default function Error() {
+  return (
+    <div id="notfound">
+		<div class="notfound">
+			<div class="notfound-404">
+				<h1>404</h1>
+				<h2>Page not found</h2>
+			</div>
+			<Link to="/">Homepage</Link>
+		</div>
+	</div>
+  )
+}
