7b797ea54d173ccc75061d3ee259482bdb0141de
Malek Al-Sheekh Hasan <malek.alsheekh01@gmail.com>
2023-05-25 10:40:08 +0000

    fixing pull

--- src/components/Main/Section/Section.js
+++ src/components/Main/Section/Section.js
@@ -4,7 +4,7 @@ export default function Section(props) {
   return (
     <section className={`${!props.isSidebarVisible ? '' : 'full-width'}`}>
       <div className='full-section'>
-        
+
       </div>
     </section>
   )
