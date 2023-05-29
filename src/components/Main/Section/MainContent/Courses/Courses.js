c521e11184bbbb6ea3da92ee8a0cdc5075ec783d
Mohammad-Keath <m.ghyath.z@gmail.com>
2023-05-29 09:51:14 +0000

    adding announcment center

--- src/components/Main/Section/MainContent/Courses/Courses.js
+++ src/components/Main/Section/MainContent/Courses/Courses.js
@@ -56,38 +56,8 @@ const handleclose = () => {
          </div>
        </div>
       ))}
-      <div className="ag-courses_item">
-         <div  className="ag-courses-item_link">
-           <div className="ag-courses-item_bg"></div>
-   
-           <div className="ag-courses-item_title">
-            PYTHON
-           </div>
-   
-           <div className="ag-courses-item_date-box">
-             Start: 
-             <span className="ag-courses-item_date">
-                04.11.2022
-             </span>
-           </div>
-         </div>
-       </div>
-       <div className="ag-courses_item">
-         <a href="#" className="ag-courses-item_link">
-           <div className="ag-courses-item_bg"></div>
-   
-           <div className="ag-courses-item_title">
-            PYTHON
-           </div>
-   
-           <div className="ag-courses-item_date-box">
-             Start:
-             <span className="ag-courses-item_date">
-               04.11.2022
-             </span>
-           </div>
-         </a>
-       </div>
+     
+       
       </div>
       
     
